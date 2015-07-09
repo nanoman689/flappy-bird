68751356e50d8da28c927bb7dc75eb466a7a939c
Nanoman <nanoman689@gmail.com>
2015-07-08 20:41:50 -0700

    Starting up
    Got it to work, something with the browserfy script was not working
    correctly and I think it had something to do with my gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 
-var concat = require('gulp-concat');
-var minifyHTML = require('gulp-minify-html');
-var imagemin = require('gulp-imagemin');
 var jshint = require('gulp-jshint');
-var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
 var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
 // JavaScript linting task
 gulp.task('jshint', function() {
@@ -22,43 +25,46 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('site/css'));
 });
 
-// Minify index
-gulp.task('html', function() {
-  return gulp.src('site/index.html')
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('build/'));
-});
-
-// JavaScript build task, removes whitespace and concatenates all files
-gulp.task('scripts', function() {
-  return gulp.src('site/js/*.js')
-    .pipe(concat('app.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
-});
-
-// Styles build task, concatenates all the files
-gulp.task('styles', function() {
-  return gulp.src('site/css/*.css')
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('build/css'));
-});
-
-// Image optimization task
-gulp.task('images', function() {
-  return gulp.src('site/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('build/img'));
-});
-
 // Watch task
 gulp.task('watch', function() {
   gulp.watch('site/js/*.js', ['jshint']);
   gulp.watch('site/scss/*.scss', ['sass']);
 });
 
+// Minify index
+gulp.task('html', function() {
+  gulp.src('site/index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('build/'));
+});
+
+// JavaScript build task, removes whitespace and concatenates all files
+gulp.task('scripts', function() {
+  return browserify('./site/js/main.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+// Styles build task, concatenates all the files
+gulp.task('styles', function() {
+  gulp.src('site/css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+// Image optimization task
+gulp.task('images', function() {
+  gulp.src('site/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'));
+});
+
+
+
 // Default task
 gulp.task('default', ['jshint', 'sass', 'watch']);
 
-// Build task
-gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
+gulp.task('build', ['sass', 'html', 'scripts', 'styles', 'images']);
